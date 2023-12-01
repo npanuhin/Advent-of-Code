25ca800ab8c7ceb103a96462ee2e9abdcce183f3
Nikita Panyuhin <n.panuhin@mail.ru>
2023-12-01 12:26:32 +0100

    Added solution for 2023/Day 01

---
+++ 2023/Day 01/part1.py
@@ -0,0 +1,19 @@
+def find_left(line: str) -> str:
+    for i in range(len(line)):
+        if line[i].isdigit():
+            return line[i]
+
+
+def find_right(line: str) -> str:
+    for i in range(len(line) - 1, -1, -1):
+        if line[i].isdigit():
+            return line[i]
+
+
+with open('input.txt', 'r') as file:
+    lines = list(filter(None, map(str.strip, file)))
+
+print(sum(
+    int(find_left(line) + find_right(line))
+    for line in lines
+))
---
+++ 2023/Day 01/part2.py
@@ -0,0 +1,40 @@
+DIGIT_NAMES = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+}
+
+
+def find_left(line: str) -> str:
+    for i in range(len(line)):
+        if line[i].isdigit():
+            return line[i]
+
+        for name, value in DIGIT_NAMES.items():
+            if line[i:i + len(name)] == name:
+                return str(value)
+
+
+def find_right(line: str) -> str:
+    for i in range(len(line) - 1, -1, -1):
+        if line[i].isdigit():
+            return line[i]
+
+        for name, value in DIGIT_NAMES.items():
+            if line[i - len(name) + 1:i + 1] == name:
+                return str(value)
+
+
+with open('input.txt', 'r') as file:
+    lines = list(filter(None, map(str.strip, file)))
+
+print(sum(
+    int(find_left(line) + find_right(line))
+    for line in lines
+))
